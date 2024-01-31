0716323df59292b063b1a89ba4570fa91c6d978c
emilypeterson <emkpetersonn@gmail.com>
2024-01-31 09:40:41 -0700

    team description

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
 
-// this is a test - emily 
+// this is Mission 4 for group 0102, Emily Peterson, Elise Pickett, Lindsey Gordon, Patrick Nieves
+
