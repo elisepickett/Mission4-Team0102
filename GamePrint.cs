61fad7d8b786eb08777c6934e74e6d283b3c4204
pnie23 <pdnie567@gmail.com>
2024-01-31 10:35:29 -0700

    add GamePrint.cs file

---
+++ GamePrint.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mission4
+{
+    internal class GamePrint
+    {
+        public void PrintBoard()
+        {
+            //for loops to print board depending on boardSize variable
+            //needs to be updated to receive the userInput array
+            int boardSize = 3;
+            for(int i = 0; i < boardSize; i++)
+            {
+                for(int j = 0; j < boardSize; j++)
+                {
+                    Console.Write("X");
+                    Thread.Sleep(400);
+                }
+                Console.WriteLine("");
+            }
+        }
+        public void PrintWinner()
+        {
+            //figure out combinations to determine a winner
+            //use variables in combinations; variable inputs here depend on how Program.cs input is received
+            string xInput = "";
+            string oInput = "";
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,5 +1,10 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using Mission4;
+
+GamePrint gp = new GamePrint();
+
 Console.WriteLine("Hello, World!");
+
 
 // this is Mission 4 for group 0102, Emily Peterson, Elise Pickett, Lindsey Gordon, Patrick Nieves
 
