f0b32ed335972427c607e803cab06907c0a4095b
emilypeterson <emkpetersonn@gmail.com>
2024-02-01 17:59:41 -0700

    removed template code from the top

--- Program.cs
+++ Program.cs
@@ -1,12 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
-using Mission4;
-
-GamePrint gp = new GamePrint();
-
-Console.WriteLine("Hello, World!");
-
-
-// this is Mission 4 for group 0102, Emily Peterson, Elise Pickett, Lindsey Gordon, Patrick Nieves
+﻿// this is Mission 4 for group 0102, Emily Peterson, Elise Pickett, Lindsey Gordon, Patrick Nieves
 
 using Mission4;
 using System;
