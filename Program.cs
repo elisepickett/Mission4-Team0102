699426c460d77b3de13ddb76161c23c38bdc27e4
pnie23 <pdnie567@gmail.com>
2024-02-01 19:14:54 -0700

    misc

--- Program.cs
+++ Program.cs
@@ -6,26 +6,6 @@ GamePrint gp = new GamePrint();
 Console.WriteLine("Hello, World!");
 
 
-GamePrint gp = new GamePrint();
-
-Console.WriteLine("Hello, World!");
-
-
-GamePrint gp = new GamePrint();
-
-Console.WriteLine("Hello, World!");
-
-
-GamePrint gp = new GamePrint();
-
-Console.WriteLine("Hello, World!");
-
-
-GamePrint gp = new GamePrint();
-
-Console.WriteLine("Hello, World!");
-
-
 // this is Mission 4 for group 0102, Emily Peterson, Elise Pickett, Lindsey Gordon, Patrick Nieves
 
 using Mission4;
